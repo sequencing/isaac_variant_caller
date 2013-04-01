96225bcb66c9d70064028d7ab7d5de63f684bc54
Chris Saunders <csaunders@illumina.com>
2013-04-01 14:25:11 -0700

    update bam depth estimate to better handle small input samples

--- src/perl/libexec/getBamAvgChromDepth.pl
+++ src/perl/libexec/getBamAvgChromDepth.pl
@@ -99,24 +99,35 @@ while(<$FP1>) {
 
 close($FP1);
 
-my $cmd2="$samtoolsBin view -F 4 -s 0.1 $bamfile";
-my $sid=open(my $FP2,"$cmd2 |");
-
+# pass 0 is a subsampled approximation of read length,
+# if that fails, then pass 1 runs the exact computation
+#
 my $length = 0;
 my $count = 0;
-while(<$FP2>) {
-    my @F = split(/\t/,$_,7);
-    next unless($F[5] =~ /[0-9]+M/);
-    $length += $1 while($F[5] =~ /([0-9]+)M/g);
-    $count++;
-    last if($count >= 200000);
-}
-kill('INT',$sid);
-close($FP2);
+for my $pass ((0,1)) {
+    my $cmd2;
+    if($pass == 0) {
+        $cmd2="$samtoolsBin view -F 4 -s 0.1 $bamfile";
+    } else {
+        $cmd2="$samtoolsBin view -F 4 $bamfile";
+    }
 
+    my $sid=open(my $FP2,"$cmd2 |");
 
-if($count <= 100000) {
-    die("Unexpected read length approximation results");
+    $length = 0;
+    $count = 0;
+    while(<$FP2>) {
+        my @F = split(/\t/,$_,7);
+        next unless($F[5] =~ /[0-9]+M/);
+        $length += $1 while($F[5] =~ /([0-9]+)M/g);
+        $count++;
+        last if($count >= 200000);
+    }
+    kill('INT',$sid);
+    close($FP2);
+
+    last if($count > 100000);
+    logX("Poor read length approximation results. Count: '$count' Rerunning exact estimate"); # rerun pass 1 for exact read length
 }
 
 
